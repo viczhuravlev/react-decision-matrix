164b0e1f016373ec74b871b3d2a6bbea4af1303e
Victor Zhuravlev <vzhuravlev15@gmail.com>
2019-08-06 23:03:54 +0300

    fix: docs

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,11 @@
+const { NODE_ENV } = process.env;
+
+const isProduction = NODE_ENV === 'production';
+
 export default {
   typescript: true,
-  title: 'Docz Typescript',
+  title: 'React Decision Matrix',
   menu: ['Getting Started', 'Components'],
   dest: '/docs',
+  base: isProduction ? '/react-decision-matrix/' : '/',
 }
