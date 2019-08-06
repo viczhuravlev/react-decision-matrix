278f0be9f8af8ff411c53b652cb29f4fcdebb4f9
Victor Zhuravlev <vzhuravlev15@gmail.com>
2019-08-07 01:38:30 +0300

    chore(lib): config build cache

--- lib/types/Alert/Alert.d.ts
+++
@@ -1,7 +0,0 @@
-import { SFC } from 'react';
-export declare type Kind = 'info' | 'positive' | 'negative' | 'warning';
-export declare type KindMap = Record<Kind, string>;
-export interface AlertProps {
-    kind: 'info' | 'positive' | 'negative' | 'warning';
-}
-export declare const Alert: SFC<AlertProps>;
